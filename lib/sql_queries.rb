275b1d82e428f2d5758be708e2e1b53062fc397b
Andrew Bernier <a6bernier@gmail.com>
2018-11-02 17:57:06 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,10 +27,11 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(pledges.amount) - project.funding_goal AS 'Overage'
+  "SELECT projects.title, (SUM(pledges.amount) - project.funding_goal) AS 'Overage'
   FROM projects
   INNER JOIN pledges
   ON pledges.user_id = users.id
+  GROUP BY project_id HAVING SUM(pledges.amount) >= funding_goal
   "
 end
 
