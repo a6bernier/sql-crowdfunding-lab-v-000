dcd37c14f0e6b1cfff52d7803e26db055d86e467
Andrew Bernier <a6bernier@gmail.com>
2018-11-02 17:46:10 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   INNER JOIN pledges
   ON pledges.user_id = users.id
   GROUP BY users.name
-  ORDER BY users.name
+  ORDER BY users.name ASC
   "
 end
 
